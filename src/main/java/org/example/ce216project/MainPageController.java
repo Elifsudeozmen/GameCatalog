a8aebb00a0211e47e6b5d7ef49b5294c9fd0a3de
Elif Sude Ozmen <elifsude035@gmail.com>
2025-03-27 00:18:44 +0300

    uploading files by using button help button

--- src/main/java/org/example/ce216project/MainPageController.java
+++ src/main/java/org/example/ce216project/MainPageController.java
@@ -1,4 +1,56 @@
 package org.example.ce216project;
 
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.List;
+
 public class MainPageController {
+    public void onEnterFileButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a JSon File");
+        FileChooser.ExtensionFilter jsonFilter = new FileChooser.ExtensionFilter("JSON Files (*.json)", "*.json");
+        fileChooser.getExtensionFilters().add(jsonFilter);
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedFile != null) {
+            if (selectedFile.getName().endsWith(".json")) {
+                //bunu implement
+                //List<Game> games = JSONHandler.readGamesFromJson(selectedFile.getAbsolutePath());
+                System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+            } else {
+                showAlert("Invalid File", "Please select a valid JSON file!");
+            }
+        }
+        else{
+                showAlert("No File Selected", "You must select a JSON file.");
+        }
+    }
+
+    public void onHelpButton(){
+        showHelpDialog();
+
+    }
+
+    private void showAlert(String title,String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    private void showHelpDialog(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("📌 Need Help?");
+        alert.setHeaderText("Here’s how you can contact us:");
+        alert.setContentText(
+                "📧 Email: support@gameapp.com\n" +
+                        "📞 Phone: " +
+                        "🌍 Website: " +
+                        "We’re here to assist you 24/7! 😊"
+        );
+        alert.showAndWait();
+    }
 }
