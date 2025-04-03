c61ac0431f052010a084e016877168fa800d6a0a
AlaraBurce <alarahan9@gmail.com>
2025-04-03 09:44:04 +0300

    edits to homePageController and homePage.fxml

--- src/main/java/org/example/ce216project/HomePageController.java
+++ src/main/java/org/example/ce216project/HomePageController.java
@@ -1,12 +1,26 @@
 package org.example.ce216project;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.List;
 public class HomePageController {
+    @FXML
+    private TextField searchBar;
+
     public void onEnterSearch(){
 
+    }
+    public void onAddButton(){
+
+    }
+    public void onHelpButton(){
+
+    }
+    public void onExportButton(){
+        
     }
 }
