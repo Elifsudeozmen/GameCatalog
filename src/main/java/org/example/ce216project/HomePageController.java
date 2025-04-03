dbeb3e91561d53e9d302eaeadaac92205307e381
AlaraBurce <alarahan9@gmail.com>
2025-04-03 09:17:31 +0300

    edits to homePage.fxml

--- src/main/java/org/example/ce216project/HomePageController.java
+++ src/main/java/org/example/ce216project/HomePageController.java
@@ -1,4 +1,12 @@
 package org.example.ce216project;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.List;
 public class HomePageController {
+    public void onEnterSearch(){
+
+    }
 }
